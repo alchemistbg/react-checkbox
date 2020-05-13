1df9686562e7aecade6666e8b8c7f6f771de59c2
alchemistbg <alchemist_bg@abv.bg>
2020-05-13 17:22:32 +0300

    Updates

---
+++ precache-manifest.d18d2090090b614de2e6c74ff295d978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849c6fb9ecd9954f518c3694edc6539d",
+    "url": "/react-checkbox/index.html"
+  },
+  {
+    "revision": "ddf08a8d17fd1736f6b9",
+    "url": "/react-checkbox/static/css/main.5ab16a75.chunk.css"
+  },
+  {
+    "revision": "b5697d52d40049118b50",
+    "url": "/react-checkbox/static/js/2.b8ca5b9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-checkbox/static/js/2.b8ca5b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf08a8d17fd1736f6b9",
+    "url": "/react-checkbox/static/js/main.cb2b9447.chunk.js"
+  },
+  {
+    "revision": "19b68b3052f35e7a4bd6",
+    "url": "/react-checkbox/static/js/runtime-main.92718d03.js"
+  }
+]);
